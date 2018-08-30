efa2034ca27b9b0876e874832f87cc1572e7f86d
Avery Freese <averyfreese@utexas.edu>
2018-08-30 15:25:28 -0500

    Update connectToDatabase.php

--- connectToDatabase.php
+++ connectToDatabase.php
@@ -4,20 +4,18 @@ function ConnectToDabase()
 {
 	/* Get environment variables (defined in Azure App Service Settings) for Azure SQL Database connection */
 	/* Further info: https://docs.microsoft.com/en-us/azure/app-service-web/web-sites-configure#application-settings */
-	$serverName = "averyfreeseserver.database.windows.net"; // In the form of: sqlservername.database.windows.net
+	$serverName = getenv("msSqlServer"); // In the form of: sqlservername.database.windows.net
 	$connectionOptions = array(
-		"Database" => "Vehicles",
-		"Uid" => "averyfreese",
-		"PWD" => "Preston1994!"
-	);
-
-	// Connect to Azure SQL Database
-	$conn = sqlsrv_connect($serverName, $connectionOptions);
-	if (!$conn) {
-   die("Connection failed: " . mysqli_connect_error());
+   	"Database" => getenv("databaseName"),
+    	"Uid" => getenv("dbUserName"),
+   	"PWD" => getenv("dbPassword")
+  );
+  // Connect to Azure SQL Database
+  $conn = sqlsrv_connect($serverName, $connectionOptions);
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
-  echo "Connected successfully";
-
-	return $conn;
+echo "Connected successfully";
+  return $conn;
 }
-?>
+?>  
